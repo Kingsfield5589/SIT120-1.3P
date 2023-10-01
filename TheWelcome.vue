6f62e31b10dad216c3fc8b619c20f2de252501f8
Kingsfield5589 <139520244+Kingsfield5589@users.noreply.github.com>
2023-10-01 11:08:31 +0800

    Add files via upload

---
+++ TheWelcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class ="welcome-line">
+        <h1>Welcome to my personal website</h1>
+    </div>
+</template>
+
+
+<style scoped>
+.welcome-line {
+    text-align: center;
+    font-display: "Arial", sans-serif;
+    color: rgb(252, 195, 142);
+};
+</style>
